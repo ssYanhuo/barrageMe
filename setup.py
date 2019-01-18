2a4af33f850f859013c560d059e19360842c26ac
ssYanhuo <wangjianghan@qq.com>
2019-01-18 22:26:24 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys , datetime
+
+def div():
+    #分隔线
+    print()
+    print("+--------------------------------------------+")
+    print()
+
+div()
+print("Hello,there.")
+print("Here is BarrageMe, a simple and lightweight application to show barrages on your screen.")
+print("Author:ss_Yanhuo")
+print("This project is NOT FINISHED YET, when it finished, this python script will help you finish BarrgeMe's necessary environment setup.")
+print("Now this project only adapted to 1920x1080 screen, and a 1280x800 extra screen.")
+print("To try it now, run '\python\BarrageMe_1.0.0.py'")
+div()
+print("Thanks to your waiting...")
